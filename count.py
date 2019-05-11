b8dbbe41f57f15ec47b2df66ef0c871c36e07d51
jerrymajerry <49050142+jerrymajerry@users.noreply.github.com>
2019-05-11 12:47:11 +0800

    Add files via upload

--- count.py
+++ count.py
@@ -3,7 +3,7 @@ counter=0
 with open (r'log_files\201811123021.log',encoding='utf-8')as f:
     for line in f:
         student=line.split(',')[1]
-        student_id=student[10:]
-        if student_id=='23021':
+        student_id=student[7:]
+        if student_id=='811123021':
             counter=counter+1
 print(counter)
